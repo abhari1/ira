5ea19e8f36f82b9a51b51c4821f3efb5008d3013
abhari1 <48124076+abhari1@users.noreply.github.com>
2019-03-02 12:47:13 +0330

    Add files via upload

---
+++ Simple.py
@@ -0,0 +1,9 @@
+num1 = int(input("first"))
+num2 = int(input("second"))
+op = input("operator")
+if (op== "+"):
+    print(num1+num2)
+elif(op=="-"):
+    print(num1-num1)
+else:
+    print("invalid operator")
